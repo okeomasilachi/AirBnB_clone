d96873c905a1fa76e95d7d4640cf801c681c9486
Ossai6 <calebossaiakpe@gmail.com>
2023-10-09 07:53:17 +0000

    I created the readme, author, and base_model files

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module contains the BaseModel class """
+
+import uuid
+import datetime
+
+class BaseModel:
+    """ This represents the BaseModel class """
+    def __init__(self):
+        """ This initializes an object of the BaseModel class """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """ Returns the string representation of an object of the BaseModel class """
+        return f"[{__class__.__name__}] ({self.id}) ({self.__dict__})"
+
+    def save(self):
+        """ Updates the public instance attribute updated_at with the current datetime """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing key/values of __dict__ of the instance """
+        return {
+                "__class__": __class__.__name__,
+                "updated_at": self.updated_at.isoformat(),
+                "created_at": self.created_at.isoformat(),
+                "id": self.id
+                }
+            
